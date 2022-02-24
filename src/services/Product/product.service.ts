2bcef1d0e19d6607f3d3d6c4768014324231cdb7
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-02-24 18:18:42 -0300

    add metrics

--- src/services/Product/product.service.ts
+++ src/services/Product/product.service.ts
@@ -4,15 +4,34 @@ import {
   ProductDocument,
   ProductInput,
 } from '../../models/Product/ProductModelInterfaces'
+import { databaseResponseTimeHistrogram } from '../../utils/Metrics'
 
 export const CreateProduct = async (input: ProductInput) => {
-  return await ProductModel.create(input)
+  const metricsLabels = {
+    operation: 'CreateProduct',
+  }
+  const timer = databaseResponseTimeHistrogram.startTimer()
+
+  const product = await ProductModel.create(input)
+
+  timer({ ...metricsLabels, success: 1 })
+
+  return product
 }
 export const FindProduct = async (
   query: FilterQuery<ProductDocument>,
   options: QueryOptions = { lean: true }
 ) => {
+  const metricsLabels = {
+    operation: 'FindProduct',
+  }
+
+  const timer = databaseResponseTimeHistrogram.startTimer()
+
   const product = await ProductModel.findOne(query, {}, options)
+
+  timer({ ...metricsLabels, success: 1 })
+
   if (!product) throw { message: 'Product not found', status: 404 }
   return product
 }
